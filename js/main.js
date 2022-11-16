2fec3edbbcf7cb5b4c025a4890c83e3bf5d6217b
Ahmad AlBashash <75222715+AhmadBsh@users.noreply.github.com>
2022-11-16 13:30:09 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+let m =  new Date("March 10, 2023 23:59:59")
+let now = new Date()
+let months = (m.getFullYear() - now.getFullYear()) * 12;
+months -= now.getMonth();
+months += m.getMonth();
+months= months <= 0 ? 0 : months;
+
+let years=( m.getFullYear() - now.getFullYear()-months<0?0:m.getFullYear() - now.getFullYear());
+
+
+let countDownDate = new Date("March 10, 2023 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  days=Math.floor(days/months)
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector("#year").innerHTML = years < 10 ? `0${years}` : years;
+  document.querySelector("#month").innerHTML = months < 10 ? `0${months}` : months;
+  document.querySelector("#day").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector("#hour").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector("#minute").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
