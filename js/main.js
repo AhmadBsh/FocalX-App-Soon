51bc2559c601166692a43fa2a0bdfd78e104f38c
aya killiah <ayakilliah2163@gmail.com>
2022-11-17 12:31:04 +0200

    edit counter

--- js/main.js
+++ js/main.js
@@ -6,23 +6,21 @@ months -= now.getMonth();
 months += m.getMonth();
 months= months <= 0 ? 0 : months;
 
-let years=( m.getFullYear() - now.getFullYear()-months<0?0:m.getFullYear() - now.getFullYear());
-
-
 let countDownDate = new Date("Dec 24, 2022 09:57:00").getTime();
 
 let counter = setInterval(() => {
   // Get Date Now
-
+  let nowDate = new Date().getTime()
   // Find The Date Difference Between Now And Countdown Date
-  let dateDiff = countDownDate - now;
+  let dateDiff = countDownDate - nowDate;
+
   let days = Math.floor((dateDiff % (1000 * 60 * 60 * 24*30)) / (1000 * 60 * 60*24));
   let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   hours=hours<=0?0:hours;
   let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
   minutes=minutes<=0?0:minutes;
   let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-  seconds=seconds<=0 ? 0 : seconds;
+  seconds=seconds<=0?0:seconds;
 
   document.querySelector("#month").innerHTML = months < 10 ? `0${months}` : months;
   document.querySelector("#day").innerHTML = days < 10 ? `0${days}` : days;
