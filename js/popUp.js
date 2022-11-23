15599a4953b7e2e50d4b7f767918d9be7afac764
Ahmad AlBashash <75222715+AhmadBsh@users.noreply.github.com>
2022-11-23 15:04:23 +0200

    changed let to var

--- js/popUp.js
+++ js/popUp.js
@@ -1,8 +1,8 @@
-let close = document.querySelector('#close')
-let popUpMsg = document.querySelector('#popUp')
-let subscribe = document.querySelector('#subscribe_Submit')
-let bg_img__c = document.querySelector('.bg_img')
-let containerr = document.querySelector('#container')
+var close = document.querySelector('#close')
+var popUpMsg = document.querySelector('#popUp')
+var subscribe = document.querySelector('#subscribe_Submit')
+var bg_img__c = document.querySelector('.bg_img')
+var containerr = document.querySelector('#container')
 close.addEventListener('click',()=>{
     popUpMsg.style.display = 'none'
     bg_img__c.classList.remove('blur')
